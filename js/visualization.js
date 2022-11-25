9827641eab71d48d887b50a27d8f88b012c343c1
Jasperhino <blum.jasper@gmail.com>
2022-11-25 17:05:18 +0100

    Initial commit

---
+++ js/visualization.js
@@ -0,0 +1,238 @@
+d3.csv("data/tracks_filtered_jazz.csv", (track) => {
+  return d3.autoType({
+    id: track.id,
+    artists: track.artists,
+    id_artists: track.id_artists,
+    name: track.name,
+    year: track.year,
+    tempo: track.tempo,
+  });
+}).then(useData);
+
+function useData(data) {
+  const apiToken =
+    "BQAw3EPkOOXfmC_1aw7KdEmo1LoDIKeQrkgrATgsICQBsjgyX5HJ-QFjQrxbSAUfc2KSZujnnYjLBlUvUmnjVsJNM-EsOIM0ANT4hH9-XWNlNIYC28DprptpalHLvSxjAitmcbd8Sv59UiFDl5uCfR9yNhw7RmKi660FCkhbfmI";
+  const categories = [
+    "tempo",
+    "duration",
+    "loudness",
+    "energy",
+    "valence",
+    "acousticness",
+  ];
+  const primaryColor = "#69b3a2";
+
+  let selectedCategory = "tempo";
+  let selectedTrack = null;
+
+  const width = 1600;
+  const height = 800;
+
+  console.log(
+    "extent:",
+    d3.extent(data, (d) => d.year)
+  );
+
+  const timeScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d.year))
+    .range([0, width]);
+  console.log(timeScale(1920));
+
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, (d) => d[selectedCategory]))
+    .nice()
+    .range([0, height]);
+
+  console.log(
+    "tempoextend",
+    d3.extent(data, (d) => d.tempo)
+  );
+
+  // console.log(
+  //   data.map((d) => {
+  //     console.log("original", d.artists);
+  //     const parts = d.artists.replace("[", "").replace("]", "").split(", ");
+  //     console.log("parts", parts);
+  //     const json = parts.map((p) => {
+  //       const escaped = p.replace(/'/g, "\x27").replace(/"/g, "\x22");
+
+  //       console.log("escaped", escaped);
+  //       return JSON.parse(escaped);
+  //     });
+  //     //const json = JSON.parse(d.artists); //d.artists.replace(/'/g, '"'));
+  //     console.log("javascript", json);
+  //   })
+  // );
+
+  const n_timebins = 100;
+  const time_bin = d3
+    .bin()
+    .value((d) => d.year)
+    .thresholds(n_timebins)
+    .domain(timeScale.domain());
+  const time_bins = time_bin(data);
+
+  const time_bins_sorted = time_bins.map((d) => {
+    d.sort((a, b) => a[selectedCategory] - b[selectedCategory]);
+    return d;
+  });
+
+  var histogram = d3
+    .histogram()
+    .value((d) => d[selectedCategory])
+    .domain(yScale.domain())
+    .thresholds(70);
+
+  console.log(
+    "histograms",
+    time_bins.map((d) => histogram(d))
+  );
+
+  console.log("bins", time_bins);
+
+  console.log("bins_sorted", time_bins_sorted);
+
+  console.log(data);
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height]);
+
+  const tooltip = d3
+    .select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("visibility", "visible")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+    .on("mouseout", (e) => {
+      d3.select(e.currentTarget)
+        .style("z-index", 0)
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+        .style("visibility", "hidden");
+    });
+
+  tooltip
+    .append("img")
+    .attr(
+      "src",
+      "https://github.com/holtzy/D3-graph-gallery/blob/master/img/section/ArcSmal.png?raw=true"
+    )
+    .attr("width", 100)
+    .attr("height", 100);
+  tooltip.append("h2").text("Title");
+  tooltip.append("p").text("Description");
+
+  const wilkinsons = svg
+    .selectAll(null)
+    .data(time_bins_sorted)
+    .enter()
+    .append("g")
+    .attr("transform", (d) => `translate(${timeScale(d.x0)}, ${height})`);
+
+  const dots = wilkinsons
+    .selectAll("dot")
+    .data((d) => {
+      const radius = (timeScale(d.x1) - timeScale(d.x0)) / 8;
+      return d.map((p, i) => ({
+        idx: i,
+        id: p.id,
+        name: p.name,
+        value: p.tempo,
+        radius,
+        artists: p.artists,
+        id_artists: p.id_artists,
+        y: -i * 2 * radius - radius,
+      }));
+    })
+    .enter()
+    .append("circle")
+    .attr("cx", (d, i) => 0)
+    .attr("cy", (d) => d.y)
+    .attr("r", (d) => d.radius)
+    .attr("fill", primaryColor)
+    .on("click", (e, d) => {
+      selectedTrack = d;
+      console.log("selectedTrack", selectedTrack);
+      d3.select(e.currentTarget)
+        .transition()
+        .duration("200")
+        .attr("r", d.radius * 2)
+        .attr("fill", "black");
+
+      //retrive cover image using spotify api
+      const url = `https://api.spotify.com/v1/tracks/${d.id}`;
+      fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${apiToken}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const cover = data.album.images[0].url;
+          tooltip.select("img").attr("src", cover);
+        });
+
+      tooltip
+        .style("left", e.offsetX + 30 + "px")
+        .style("top", e.offsetY + "px")
+        .style("visibility", "visible");
+
+      tooltip.select("h2").text(`${d.name} - ${d.artists}`);
+      tooltip.select("p").text(`${selectedCategory}: ${d.value}`);
+
+      tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 1)
+        .style("visibility", "visible");
+    })
+    .on("mouseover", (e, d) => {
+      d3.select(e.currentTarget)
+        .transition()
+        .duration("200")
+        .attr("r", d.radius * 1.5)
+        .attr("fill", "black");
+    })
+
+    .on("mouseout", (e, d) => {
+      d3.select(e.currentTarget)
+        .transition()
+        .duration("200")
+        .attr("r", d.radius);
+    });
+
+  const globalScale = width / 5000;
+  const extent = [
+    [0, 0],
+    [width, 0],
+  ];
+  const scaleExtent = [width / 5000, 2]; // Infinity]
+  const translateExtent = [
+    [0, 0],
+    [5000, 0],
+  ];
+  const zoom = d3
+    .zoom()
+    .extent(extent)
+    .scaleExtent(scaleExtent)
+    .translateExtent(translateExtent)
+    .on("zoom", (e) => {
+      console.log(e);
+      const { k, x, y } = e.transform;
+      console.log("zoom", k, x, y);
+    });
+
+  svg.call(zoom);
+  svg.call(zoom.scaleBy, globalScale);
+}
---
+++ preprocessing.py
@@ -0,0 +1,104 @@
+# %%
+import ast
+import pandas as pd
+import numpy as np
+
+# %%
+a = pd.read_csv('data/artists.csv')
+# artists with over 80 popularity
+a = a[a['popularity'] > 90]
+# sort by popularity
+#%%
+a.sort_values(by=['popularity'], inplace=True, ascending=False)
+# select top 0.1%
+top = a.head(int(len(a)*(0.01/100)))
+
+# %%
+# load csv file to dataframe
+df = pd.read_csv('data/tracks.csv', dtype={"id":"string"}, converters={'id_artists':lambda x: np.array(ast.literal_eval(x))})
+#%%
+filtered = df[df['duration_ms'] > 15 * 60 * 1000]
+print(len(filtered) / len(df) * 100)
+filtered.head(100)
+# %%
+# filter all songs longer than 15 minutes
+#df = df[df['duration_ms'] < 15 * 60 * 1000]
+# transform release_date to year
+df['year'] = df['release_date'].apply(lambda x: x.split('-')[0])
+df = df.astype({'year':'int'})
+df.reset_index()
+# filter all songs with decade < 1960 and > 2010
+df = df[(df['year'] >= 1920)]
+df
+#%%
+
+def artist_exists(row, artists):
+    for artist in row['id_artists']:
+        if artist in artists:
+            return True
+    return False;
+
+artists = set(a['id'])
+mask = [artist_exists(row, artists) for _, row in df.iterrows()]
+# %%
+filtered_by_popular_artists = df[mask]
+filtered_by_popular_artists
+# %%
+# order by year
+filtered_by_popular_artists = filtered_by_popular_artists.sort_values(by='year')
+# %%
+#%%
+filtered_by_popular_artists['year'].value_counts(sort=False).plot(kind='bar', figsize=(20, 10))
+
+#%%
+
+# group by decade and sort by popularity and select top 100 for every decade
+df = df.groupby('year').apply(lambda x: x.sort_values(
+    'popularity', ascending=False).head(100))
+df = df.reset_index(drop=True)
+df
+# %%
+# How many rows have tempo 0
+df.loc[df['tempo'] == 0].count()
+
+# %%
+
+# save to csv
+filtered_by_popular_artists.to_csv('data/tracks_filtered.csv', index=False, header=True)
+
+# %%
+df.loc[df['populartity'] == 0].count()
+
+
+# %%
+#filter artists where genre contains 'jazz'
+a = pd.read_csv('data/artists.csv')
+
+jazz_artists = a[a['genres'].str.contains('jazz')]
+jazz_artists.sort_values(by=['popularity'], inplace=True, ascending=False)
+# select top 0.1%
+#top_jazz_artists = a.head(int(len(jazz_artists)*(50/100)))
+#remove artists with popularity < 10
+top_jazz_artists = jazz_artists[jazz_artists['popularity'] > 65]
+
+top_jazz_artists
+#%%
+#histogram of followers
+top_jazz_artists['followers'].plot(kind='hist', bins=50, figsize=(20, 10))
+# %%
+
+set_jazz_artists = set(top_jazz_artists['id'])
+mask = [artist_exists(row, set_jazz_artists) for _, row in df.iterrows()]
+
+# %%
+filtered_by_jazz_artists = df[mask]
+filtered_by_jazz_artists
+# %%
+filtered_by_jazz_artists.sort_values(by='year', inplace=True)
+
+filtered_by_jazz_artists['year'].value_counts(sort=False).plot(kind='bar', figsize=(20, 10))
+
+# %%
+filtered_by_jazz_artists.to_csv('data/tracks_filtered_jazz.csv', index=False, header=True)
+
+# %%
